9264102676f1633720c3682c9c4f2e127ded81d0
Jayden Pyles <jpylesbusiness@gmail.com>
2024-06-15 13:14:22 -0500

    feat: upload notes to uploader

--- main.ts
+++ main.ts
@@ -1,89 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface PluginSettings {
+	url: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	url: "url",
+};
+
+interface Note {
+	name: string | null;
+	tags: string[];
+	content: string | null;
+	group: string | null;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const extractContentAfterTags = (content: string) => {
+	const regex = /---\n#[^\n]*\n\n([\s\S]*)/;
+	const matches = content.match(regex);
+
+	if (matches) {
+		return matches[1].trim();
+	}
+
+	return content;
+};
+
+export default class NoteUploaderPlugin extends Plugin {
+	settings: PluginSettings;
+	uploadNote = async (note: Note) => {
+		await fetch(this.settings.url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(note),
+		})
+			.then(async (res) => {
+				return res.status;
+			})
+			.then((res) => {
+				if (res === 200) {
+					new Notice("Note successfully uploaded.");
+				} else {
+					new Notice("Something went wrong when uploading!");
+				}
+			});
+	};
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.addRibbonIcon(
+			"upload",
+			"Upload to Very Simple Notes",
+			async (evt: MouseEvent) => {
+				const activeFile = this.app.workspace.getActiveFile();
+				const filename = activeFile ? activeFile.name : null;
+				let content = null;
+				let tags: string[] = [];
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+				if (activeFile) {
+					content = await this.app.vault.cachedRead(activeFile);
+					content = extractContentAfterTags(content);
+					const _tags =
+						this.app.metadataCache.getFileCache(activeFile)?.tags;
+					if (_tags) {
+						tags = _tags.map((tag) => tag.tag.replace("#", ""));
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
-		});
+
+				const note = {
+					name: filename,
+					content: content,
+					tags: tags,
+					group: null,
+				};
+
+				new UploadModal(this.app, this.uploadNote, note).open();
+			},
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData(),
+		);
 	}
 
 	async saveSettings() {
@@ -91,44 +118,64 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class UploadModal extends Modal {
+	result: string;
+	onSubmit: (note: Note) => void;
+	note: Note;
+
+	constructor(app: App, onSubmit: (note: Note) => void, note: Note) {
 		super(app);
+		this.onSubmit = onSubmit;
+		this.note = note;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		contentEl.createEl("h1", { text: "Uploading note" });
+
+		new Setting(contentEl).setName("Group").addText((text) =>
+			text.onChange((value) => {
+				this.result = value;
+			}),
+		);
+
+		new Setting(contentEl).addButton((btn) =>
+			btn
+				.setButtonText("Submit")
+				.setCta()
+				.onClick(() => {
+					this.close();
+					this.note.group = this.result;
+					this.onSubmit(this.note);
+				}),
+		);
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SettingTab extends PluginSettingTab {
+	plugin: NoteUploaderPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NoteUploaderPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		new Setting(containerEl).setName("Upload url").addText((text) =>
+			text.setValue(this.plugin.settings.url).onChange(async (value) => {
+				this.plugin.settings.url = value;
+				await this.plugin.saveSettings();
+			}),
+		);
 	}
 }
